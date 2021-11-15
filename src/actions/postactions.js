0f2c700d60b236f65bfbd891e39423151057667d
santhoshhazare1 <you@example.com>
2021-11-15 10:06:07 +0530

    child_branch

--- src/actions/postactions.js
+++ src/actions/postactions.js
@@ -3,7 +3,7 @@ export const GET_POSTS = 'GET_POSTS'
 export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'
 export const GET_POSTS_FAILURE = 'GET_POSTS_FAILURE'
 
-// Create Redux action creators that return an action
+
 export const getPosts = () => ({
     type: GET_POSTS,
   })
